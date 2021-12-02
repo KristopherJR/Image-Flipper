bc3667d66b7e43e8eec587ac8a0979e5cff76b52
KristopherJR <kristopher.randle@outlook.com>
2021-12-02 16:05:19 +0000

    Version 1.0. User can load in an image from the File Explorer, and is limited to 10 .PNG type images. These are displayed in a form and the user can select an image by clicking it. The selection functionality still requires some work.

---
+++ ImageFlipper/Client/ImageGallery.Designer.cs
@@ -0,0 +1,111 @@
+﻿
+namespace Client
+{
+    partial class ImageGallery
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.LoadImageButton = new System.Windows.Forms.Button();
+            this.EditImageButton = new System.Windows.Forms.Button();
+            this.ExitButton = new System.Windows.Forms.Button();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // LoadImageButton
+            // 
+            this.LoadImageButton.Location = new System.Drawing.Point(12, 415);
+            this.LoadImageButton.Name = "LoadImageButton";
+            this.LoadImageButton.Size = new System.Drawing.Size(75, 23);
+            this.LoadImageButton.TabIndex = 10;
+            this.LoadImageButton.Text = "Load Image";
+            this.LoadImageButton.UseVisualStyleBackColor = true;
+            this.LoadImageButton.Click += new System.EventHandler(this.LoadImageButton_Click);
+            // 
+            // EditImageButton
+            // 
+            this.EditImageButton.Location = new System.Drawing.Point(413, 406);
+            this.EditImageButton.Name = "EditImageButton";
+            this.EditImageButton.Size = new System.Drawing.Size(100, 40);
+            this.EditImageButton.TabIndex = 11;
+            this.EditImageButton.Text = "Edit Image";
+            this.EditImageButton.UseVisualStyleBackColor = true;
+            // 
+            // ExitButton
+            // 
+            this.ExitButton.Location = new System.Drawing.Point(797, 12);
+            this.ExitButton.Name = "ExitButton";
+            this.ExitButton.Size = new System.Drawing.Size(75, 23);
+            this.ExitButton.TabIndex = 12;
+            this.ExitButton.Text = "Exit";
+            this.ExitButton.UseVisualStyleBackColor = true;
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(45, 45);
+            this.flowLayoutPanel1.Margin = new System.Windows.Forms.Padding(0);
+            this.flowLayoutPanel1.MaximumSize = new System.Drawing.Size(800, 350);
+            this.flowLayoutPanel1.MinimumSize = new System.Drawing.Size(800, 350);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(800, 350);
+            this.flowLayoutPanel1.TabIndex = 14;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(190, 415);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // ImageGallery
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(884, 461);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Controls.Add(this.ExitButton);
+            this.Controls.Add(this.EditImageButton);
+            this.Controls.Add(this.LoadImageButton);
+            this.Name = "ImageGallery";
+            this.Text = "Image Gallery";
+            this.Load += new System.EventHandler(this.ImageGallery_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button LoadImageButton;
+        private System.Windows.Forms.Button EditImageButton;
+        private System.Windows.Forms.Button ExitButton;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
---
+++ ImageFlipper/Client/ImageGallery.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Client
+{
+    /// <summary>
+    /// ImageGallery allows a user to load in images for manipulation.
+    /// 
+    /// Author: Kristopher Randle
+    /// Version: 02/12/2021
+    /// 
+    /// OpenFileDialog code from: https://www.c-sharpcorner.com/UploadFile/mirfan00/uploaddisplay-image-in-picture-box-using-C-Sharp/
+    /// </summary>
+    public partial class ImageGallery : Form
+    {
+        private Dictionary<string,Image> _images;
+        private Dictionary<string, PictureBox> _pictureBoxes;
+        private PictureBox _selectedPictureBox;
+        private PictureBox _previouslySelectPictureBox;
+        public ImageGallery()
+        {
+            InitializeComponent();    
+        }
+
+        private void ImageGallery_Load(object sender, EventArgs e)
+        {
+            // INITIALISE the Dictionaries:
+            _images = new Dictionary<string, Image>();
+            _pictureBoxes = new Dictionary<string, PictureBox>();
+
+        }
+        /// <summary>
+        /// Mouse click handler for each new image that is created. Allows an image to be selected.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void newPicture_Click(object sender, MouseEventArgs e)
+        {
+            PictureBox clickedBox = sender as PictureBox;
+
+            // if no box is selected
+            if (_selectedPictureBox == null && _previouslySelectPictureBox == null)
+            {
+                // select the clicked box
+                _selectedPictureBox = clickedBox;
+                _previouslySelectPictureBox = clickedBox;
+                _selectedPictureBox.BackColor = Color.FromArgb(142, 205, 117);
+                return;
+            }
+
+            if (_selectedPictureBox == clickedBox)
+            {
+                _selectedPictureBox.BackColor = Color.Empty;
+                _previouslySelectPictureBox.BackColor = Color.Empty;
+                _selectedPictureBox = null;
+                _previouslySelectPictureBox = null;
+                return;
+            }
+
+            if (_selectedPictureBox != clickedBox)
+            {
+                _previouslySelectPictureBox = _selectedPictureBox;
+                _selectedPictureBox = clickedBox;
+
+                _previouslySelectPictureBox.BackColor = Color.Empty;
+                _selectedPictureBox.BackColor = Color.FromArgb(142, 205, 117);
+            }
+        }
+        /// <summary>
+        /// Called each time the Load Image button is clicked.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void LoadImageButton_Click(object sender, EventArgs e)
+        {
+            // CHECK that the user hasn't already loaded 10 images:
+            if(_images.Count < 10)
+            {
+                // CREATE a new OpenFileDialog so that the user can load an image from the File Explorer:
+                OpenFileDialog open = new OpenFileDialog();
+
+                // DEFINE images filters to restrict the user to only .PNG file formats:
+                open.Filter = "Image Files(*.png;)|*.png;";
+                // OPEN the File Explorer and wait for the user to select an image file:
+                if (open.ShowDialog() == DialogResult.OK)
+                {
+
+                    // CHECK that the image selected has NOT already been loaded:
+                    if (!_images.ContainsKey(open.FileName))
+                    {
+                        // ADD the image to the _images Dictionary. Use the FileName as the key:
+                        _images.Add(open.FileName, Image.FromFile(open.FileName));
+                        // CREATE a new PictureBox, call it newPicture:
+                        PictureBox newPicture = new PictureBox();
+                        // CREATE a mouse click handler for the newPicture:
+                        
+                        
+                        newPicture.MouseClick += new MouseEventHandler(newPicture_Click);
+                        // ADD newPicture to the _pictureBoxes dict. Use the same FileName as the key:
+                        _pictureBoxes.Add(open.FileName, newPicture);
+                        // SET its Size and SizeMode:
+                        newPicture.SizeMode = PictureBoxSizeMode.CenterImage;
+                        newPicture.Size = new Size(150, 150);
+
+                        // ADD newPicture to the flowLayoutPanel:
+                        flowLayoutPanel1.Controls.Add(newPicture);
+
+                        // display the image in the picture box:
+                        newPicture.Image = new Bitmap(open.FileName);
+                        // image file path  
+                        textBox1.Text = open.FileName;
+                    }
+                    else
+                    {
+                        // IF the user tries to load a previously loaded image, print an error message:
+                        Console.WriteLine("Sorry! You've already loaded that image.");
+                    }
+
+
+                }
+
+            }
+            else
+            {
+                Console.WriteLine("Sorry fam, only 10 images allowed at a time.");
+            }
+            
+        }
+    }
+}
