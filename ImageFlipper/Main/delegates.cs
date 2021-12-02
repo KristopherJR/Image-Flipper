809674d62da75732ccc8eaf0b4d8d08a610026d0
KristopherJR <kristopher.randle@outlook.com>
2021-12-02 23:31:16 +0000

    Pre-refactor backup

---
+++ ImageFlipper/Main/delegates.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/// <summary>
+/// Author: Kristopher Randle
+/// Version: 0.1, 02-12-21
+/// </summary>
+namespace Main
+{
+    
+    
+}
